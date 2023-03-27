d37dc034967bd476609abfa322eb7ddee28c762a
בני סולומון <mr.benny.solomon@gmail.com>
2023-03-27 13:47:25 +0300

    first working test

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const dotenv = require("dotenv");
+const express = require("express");
+const connectDB = require("./config/db");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+
+
+app.use(cors());
+
+app.use(express.json());
+
+const server = http.createServer(app);
+
+dotenv.config({ path: './config/config.env' });
+
+// connectDB();
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+});
